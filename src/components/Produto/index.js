1728793d8da9bd166bc8f7b2c151538c29e7b3f1
devHikary <wrkkarina@gmail.com>
2021-11-04 21:03:36 -0300

    adicionar quantidade

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,9 +1,9 @@
 import { Container } from './styles';
-import { memo } from 'react';
+import { memo, useContext } from 'react';
 import { IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-
+import { CarrinhoContext } from '../../contexts/Usuario'
 
 function Produto({
   nome,
@@ -12,28 +12,52 @@ function Produto({
   valor,
   unidade
 }) {
+  const {carrinho, setCarrinho } = useContext(CarrinhoContext)
+
+  function adicionaProduto(novoProduto) {
+    const temOProduto = carrinho.some(item => item.id === novoProduto.id)
+
+    if(!temOProduto){
+      novoProduto.quantidade = 1;
+      return setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto])
+    }
+    setCarrinho(carrinhoAnterior => carrinhoAnterior.map( item => {
+      if(item.id === novoProduto.id){
+        item.quantidade += 1
+      }
+      return item
+    }))
+  }
+
   return (
-      <Container>
-        <div>
-          <img
-            src={`/assets/${foto}.png`}
-            alt={`foto de ${nome}`}
-          />
-          <p>
-            {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
-          </p>
-        </div>
-        <div>
-          <IconButton
-            color="secondary"
-          >
-            <RemoveIcon />
-          </IconButton>
-          <IconButton>
-            <AddIcon />
-          </IconButton>
-        </div>
-      </Container>
+    <Container>
+      <div>
+        <img
+          src={`/assets/${foto}.png`}
+          alt={`foto de ${nome}`}
+        />
+        <p>
+          {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
+        </p>
+      </div>
+      <div>
+        <IconButton
+          color="secondary"
+        >
+          <RemoveIcon />
+        </IconButton>
+        <IconButton
+          onClick={() => adicionaProduto({
+            nome,
+            foto,
+            id,
+            valor,
+          })}
+        >
+          <AddIcon />
+        </IconButton>
+      </div>
+    </Container>
   )
 }
 
